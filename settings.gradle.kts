7ad864215c5651db48973044111a8c20f2dc5777
liuzhe828-ux <liuzhe828@gmail.com>
2026-06-07 10:22:38 +0800

    update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,18 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        google { content { includeGroupByRegex("com\\.android.*") } }
-        mavenCentral()
         gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
         google()
         mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
-
 rootProject.name = "PetDiary"
 include(":app")
